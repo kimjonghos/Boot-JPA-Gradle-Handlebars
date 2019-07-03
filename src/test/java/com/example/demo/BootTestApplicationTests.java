62d5fe8b54459989f3c7250dc7dbfc29f2d88e43
kimjonghos <olikuj933@gmail.com>
2019-07-03 23:02:23 +0900

    modify test class / nothing to do

--- src/test/java/com/example/demo/BootTestApplicationTests.java
+++ src/test/java/com/example/demo/BootTestApplicationTests.java
@@ -3,14 +3,16 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("application") // Like this
 public class BootTestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
