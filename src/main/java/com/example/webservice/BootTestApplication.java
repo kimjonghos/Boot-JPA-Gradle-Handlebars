0c57e7fa1b03d49face7f0e3b7fb89b04b7b4729
kimjonghos <olikuj933@gmail.com>
2019-07-12 13:58:56 +0900

    main

--- src/main/java/com/example/webservice/BootTestApplication.java
+++ src/main/java/com/example/webservice/BootTestApplication.java
@@ -1,6 +1,5 @@
 package com.example.webservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,15 +9,12 @@ public class BootTestApplication {
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-			//+"..\\..\\config\\bootTest\\real-application.yml";
-			//+"D:\\my\\STUDY\\MY_WORKS\\config\\bootTest\\real-application.yml";
-            + "/home/ec2-user/app/config/bootTest/real-application.yml"; //linux
-	
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(BootTestApplication.class)
-			.properties(APPLICATION_LOCATIONS)
-			.run(args);
-		//SpringApplication.run(BootTestApplication.class, args);
-	}
+            + "/app/config/bootTest/real-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(BootTestApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 
 }
