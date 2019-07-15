df84f096817faf549e40abd1750ecadb39c866b4
kimjonghos <olikuj933@gmail.com>
2019-07-15 22:40:03 +0900

    5ta

--- src/main/java/com/example/webservice/BootTestApplication.java
+++ src/main/java/com/example/webservice/BootTestApplication.java
@@ -10,7 +10,7 @@ public class BootTestApplication {
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
 			//+"..\\..\\config\\bootTest\\real-db.yml";
-            + "/home/ec2-user/app/config/bootTest/real-application.yml";
+            + "/home/ec2-user/app/config/bootTest/real-db.yml";
 
     public static void main(String[] args) {
 
