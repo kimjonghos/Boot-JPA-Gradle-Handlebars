97a1009058dbd7a4ee23d36d6961555b429e6708
kimjonghos <olikuj933@gmail.com>
2019-08-04 16:50:12 +0900

    no message

--- src/test/java/com/example/webservice/web/WebControllerTest.java
+++ src/test/java/com/example/webservice/web/WebControllerTest.java
@@ -1,22 +1,12 @@
 package com.example.webservice.web;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=RANDOM_PORT)
-@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment=RANDOM_PORT)
+//@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
 public class WebControllerTest {
-
-	@Autowired
-	private TestRestTemplate restTemplate;
+//
+//	@Autowired
+//	private TestRestTemplate restTemplate;
 	
 //	@Test
 //	public void loading_main_page() {
